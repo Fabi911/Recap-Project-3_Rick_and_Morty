bd37960285b789429a18e9050ced36de29ee4a64
Nina Weber <weber_nina@web.de>
2024-02-22 11:05:30 +0100

    add navPagination

---
+++ components/NavPagination/NavPagination.js
@@ -0,0 +1,5 @@
+// export function navPagination() {
+//   const createPagination = document.createElement("span");
+//   createPagination.classList.add("navigation__pagination");
+//   createPagination.textContent = page + " / " + maxPage;
+// }
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { CharacterCard } from "./components/CharacterCard/CharacterCard.js";
 // import NavButton from "./components/NavButton/NavButton.js";
-// import NavPagination from "./components/NavPagination/NavPagination.js";
+// import { NavPagination } from "./components/NavPagination/NavPagination.js";
 //import searchBar from "./components/SearchBar/SearchBar.js";
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
@@ -14,12 +14,15 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
-const searchQuery = "";
+let maxPage = 42;
+let page = 1;
+let searchQuery = "";
+pagination.textContent = page + " / " + maxPage;
 
 export async function fetchCharacters() {
-  const response = await fetch("https://rickandmortyapi.com/api/character");
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character?page=${page}`
+  );
   if (!response.ok) {
     console.log("Network error");
     return null;
@@ -38,3 +41,21 @@ export async function fetchCharacters() {
 fetchCharacters();
 
 CharacterCard();
+
+// previousButton
+prevButton.addEventListener("click", (event) => {
+  if (page < maxPage) {
+    page--;
+    fetchCharacters();
+    pagination.textContent = page + " / " + maxPage;
+  }
+});
+
+//nextButton
+nextButton.addEventListener("click", (event) => {
+  if (page < maxPage) {
+    page++;
+    fetchCharacters();
+    pagination.textContent = page + " / " + maxPage;
+  }
+});
